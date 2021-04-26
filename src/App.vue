c9a858230c0d0c642314be57811aa24d86b91fdf
dufuyan03 <dufuyan03@kuaishou.com>
2021-04-26 22:48:02 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="app">
-    XXX
-  </div>
+    <div id="app-teacher-task">
+        教师任务量管理系统
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'app-teacher-task'
 }
 </script>
 
 <style scoped lang="less">
-// #app {
-//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
+#app {
+    // font-family: "Avenir", Helvetica, Arial, sans-serif;
+    // -webkit-font-smoothing: antialiased;
+    // -moz-osx-font-smoothing: grayscale;
+    // text-align: center;
+    // color: #2c3e50;
+    // margin-top: 60px;
+}
 </style>
