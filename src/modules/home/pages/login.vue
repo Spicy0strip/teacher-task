c3754b1658d9a0f30bbd45e789d6513834dbdda3
dufuyan03 <dufuyan03@kuaishou.com>
2021-04-27 22:50:59 +0800

    feat: login

--- src/modules/home/pages/login.vue
+++ src/modules/home/pages/login.vue
@@ -1,13 +1,52 @@
 <template>
-    <div>
-        login
-        <router-link to="/teacher">Go to teacher</router-link>
-        <router-link to="/">Go to /</router-link>
+    <div class="page-conponent-container_login">
+        <el-form
+            :model="formData"
+            label-width="80px"
+        >
+            <el-form-item
+                label="工号"
+                prop="jobNumber"
+            >
+                <el-input placeholder="请输入工号" v-model="formData.jobNumber"></el-input>
+            </el-form-item>
+
+            <el-form-item
+                label="密码"
+                prop="password"
+            >
+                <el-input v-model="formData.password" placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submit">确定</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 <script>
+import { Input, Form, FormItem, Button } from 'element-ui';
 export default {
     name: 'login',
+    components: {
+        ElInput: Input,
+        ElForm: Form,
+        ElFormItem: FormItem,
+        ElButton: Button,
+    },
+    data() {
+        const self = this;
+        return {
+            formData: {
+                jobNumber: '',
+                password: '',
+            }
+        };
+    },
+    methods: {
+        submit() {
+
+        }
+    }
 }
 </script>
 <style lang="less" scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ export default new Router({
             path: '/',
             name: 'index',
             component: Home,
-            redirect: '/teacher',
+            redirect: '/login',
             children: [
                 {
                     path: 'teacher',
